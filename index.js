9a36a86fa82c7e572f1fc3b70d60126d98a9d607
weihuagu <weihuagu_work@163.com>
2018-09-11 23:53:36 +0800

    从网上最开始的例子开始

---
+++ index.js
@@ -0,0 +1,20 @@
+const app = require('express')()
+const server = require('http').Server(app)
+const io=require('socket.io')(server)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+'/index.html')
+})
+io.on('connection',socket=>{
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('chat message', msg => {
+    io.emit('chat message',msg)
+    console.log('message: ' + msg)
+  })
+})
+server.listen(4000, () => {
+  console.log('The server is running: http://localhost:4000')
+})
+
